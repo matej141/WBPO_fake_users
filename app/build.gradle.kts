651b737a6ad30a276f9aae924d362012c1dff221
matej141 <79569680+matej141@users.noreply.github.com>
2024-07-11 16:41:20 +0200

    Set up DataBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,11 @@ android {
     namespace = "com.android.wbpo_fake_users"
     compileSdk = 33
 
+    buildFeatures {
+        viewBinding = true
+    }
+
+
     defaultConfig {
         applicationId = "com.android.wbpo_fake_users"
         minSdk = 24
@@ -41,6 +46,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.databinding:databinding-runtime:8.0.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
