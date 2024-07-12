857cc2082ec4b4230542109ff8b580b981a0af73
matej141 <79569680+matej141@users.noreply.github.com>
2024-07-12 20:57:33 +0200

    Add API call for fetching users in ReqresApiService

--- app/src/main/java/com/android/wbpo_fake_users/ReqresApiService.kt
+++ app/src/main/java/com/android/wbpo_fake_users/ReqresApiService.kt
@@ -1,14 +1,19 @@
 package com.android.wbpo_fake_users
 
+import com.android.wbpo_fake_users.users.retrofit.models.UsersListResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 data class RegisterRequest(val email: String, val password: String)
 data class RegisterResponse(val id: Int, val token: String)
-data class ErrorResponse(val error: String)
 
 interface ReqresApiService {
     @POST("register")
     suspend fun registerUser(@Body request: RegisterRequest): Response<RegisterResponse>
+
+    @GET("users")
+    suspend fun getUsers(@Query("page") page: Int): Response<UsersListResponse>
 }
